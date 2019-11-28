e5077ab91544b42743342a05bb79cc9562e0418d
sarah wang <sarahwang47@gmail.com>
2019-11-28 15:18:11 +0800

    change

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ App({
   onLaunch: function () {
     const host = 'https://rent-a-jedi.herokuapp.com/'
     console.log('beginning login')
-
- 04d6e915a6213a741f7b21f1c576db0b8e35603a
     wx.login({
       success: (res) => {
         console.log('result of initial login', res)
